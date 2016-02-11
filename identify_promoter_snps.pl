1f7cea40b6f68f90e02377b827fe15215c297011
LILLIE LAB <rfrase03@uoguelph.ca>
2016-02-11 13:21:39 -0500

    original commit

---
+++ identify_promoter_snps.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+open (my $vcf_file, "<", $ARGV[0]);
+open (my $bed_file, "<", $ARGV[1]);
+open (my $output, ">", $ARGV[2]);
+# 
+# die "\n\nWrong usage\n\n" if $ARGV < 3;
+
+my ($snp_position, $start_pos, $end_pos, @snps);
+
+while (my $vcf_line = <$vcf_file>) {
+
+	chomp $vcf_line;
+	
+	if ($vcf_line =~ /\#/) {
+	}
+	else {
+		my @vcf_fields = split("\t", $vcf_line);
+		push @snps, $vcf_fields[1];
+	}
+}
+
+
+while (my $bed_line = <$bed_file>) {
+	chomp $bed_line;
+
+	my @bed_fields = split("\t", $bed_line);
+	$start_pos = $bed_fields[1];
+	$end_pos = $bed_fields[2];
+	
+	foreach my $snp (@snps) {
+		if ( $snp >= $start_pos && $snp <= $end_pos) {
+			print "$bed_line | snp=$snp\n";
+		}
+	}
+}
+
+close $vcf_file;
+close $bed_file;
+close $output;
+	
+		
