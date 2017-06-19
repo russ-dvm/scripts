59e3f1e179ce476fd423bf3000fdb48d4a718a84
Russell Fraser <rfrase03@uoguelph.ca>
2017-06-19 09:16:11 -0400

    * A Manhattan plot using the my modified QQMan R code. Should display chr with appropriate widths.

---
+++ pig/manhattan_plot_qqman_version.R
@@ -0,0 +1,146 @@
+library(ggplot2)
+library(qqman)
+library(viridis)
+library(ggforce)
+
+
+###STEPHEN TURNER'S CODE FROM 
+## https://github.com/stephenturner/qqman/blob/master/R/manhattan.R
+
+manhattan <- function(x, chr="CHR", bp="BP", p="P", snp="SNP", 
+                      col=c("gray10", "gray60"), chrlabs=NULL,
+                      suggestiveline=-log10(1e-5), genomewideline=-log10(5e-8), 
+                      highlight=NULL, logp=TRUE, annotatePval = NULL, annotateTop = TRUE, ...) {
+  
+  # Not sure why, but package check will warn without this.
+  CHR=BP=P=index=NULL
+  
+  # Check for sensible dataset
+  ## Make sure you have chr, bp and p columns.
+  if (!(chr %in% names(x))) stop(paste("Column", chr, "not found!"))
+  if (!(bp %in% names(x))) stop(paste("Column", bp, "not found!"))
+  if (!(p %in% names(x))) stop(paste("Column", p, "not found!"))
+  ## warn if you don't have a snp column
+  if (!(snp %in% names(x))) warning(paste("No SNP column found. OK unless you're trying to highlight."))
+  ## make sure chr, bp, and p columns are numeric.
+  if (!is.numeric(x[[chr]])) stop(paste(chr, "column should be numeric. Do you have 'X', 'Y', 'MT', etc? If so change to numbers and try again."))
+  if (!is.numeric(x[[bp]])) stop(paste(bp, "column should be numeric."))
+  if (!is.numeric(x[[p]])) stop(paste(p, "column should be numeric."))
+  
+  # Create a new data.frame with columns called CHR, BP, and P.
+  d=data.frame(CHR=x[[chr]], BP=x[[bp]], P=x[[p]])
+  
+  # If the input data frame has a SNP column, add it to the new data frame you're creating.
+  if (!is.null(x[[snp]])) d=transform(d, SNP=x[[snp]])
+  
+  # Set positions, ticks, and labels for plotting
+  ## Sort and keep only values where is numeric.
+  #d <- subset(d[order(d$CHR, d$BP), ], (P>0 & P<=1 & is.numeric(P)))
+  d <- subset(d, (is.numeric(CHR) & is.numeric(BP) & is.numeric(P)))
+  d <- d[order(d$CHR, d$BP), ]
+  #d$logp <- ifelse(logp, yes=-log10(d$P), no=d$P)
+  if (logp) {
+    d$logp <- -log10(d$P)
+  } else {
+    d$logp <- d$P
+  }
+  d$pos=NA
+  
+  
+  # Fixes the bug where one chromosome is missing by adding a sequential index column.
+  d$index=NA
+  ind = 0
+  for (i in unique(d$CHR)){
+    ind = ind + 1
+    d[d$CHR==i,]$index = ind
+  }
+  
+  # This section sets up positions and ticks. Ticks should be placed in the
+  # middle of a chromosome. The a new pos column is added that keeps a running
+  # sum of the positions of each successive chromsome. For example:
+  # chr bp pos
+  # 1   1  1
+  # 1   2  2
+  # 2   1  3
+  # 2   2  4
+  # 3   1  5
+  nchr = length(unique(d$CHR))
+  if (nchr==1) { ## For a single chromosome
+    ## Uncomment the next two linex to plot single chr results in Mb
+    #options(scipen=999)
+    #d$pos=d$BP/1e6
+    d$pos=d$BP
+    ticks=floor(length(d$pos))/2+1
+    xlabel = paste('Chromosome',unique(d$CHR),'position')
+    labs = ticks
+  } else { ## For multiple chromosomes
+    lastbase=0
+    ticks=NULL
+    for (i in unique(d$index)) {
+      if (i==1) {
+        d[d$index==i, ]$pos=d[d$index==i, ]$BP
+      } else {
+        lastbase=lastbase+tail(subset(d,index==i-1)$BP, 1)
+        d[d$index==i, ]$pos=d[d$index==i, ]$BP+lastbase
+      }
+      # Old way: assumes SNPs evenly distributed
+      # ticks=c(ticks, d[d$index==i, ]$pos[floor(length(d[d$index==i, ]$pos)/2)+1])
+      # New way: doesn't make that assumption
+      ticks = c(ticks, (min(d[d$index == i,]$pos) + max(d[d$index == i,]$pos))/2 + 1)
+      
+    }
+    xlabel = 'Chromosome'
+    #labs = append(unique(d$CHR),'') ## I forgot what this was here for... if seems to work, remove.
+    labs <- unique(d$CHR)
+  }
+  
+  # Initialize plot
+  xmax = ceiling(max(d$pos) * 1.03)
+  xmin = floor(max(d$pos) * -0.03)
+  
+  ###MODIFIED HERE -- Direct ticks and labels into their own values and return the data frame 
+  ## The datafrmae (d) can then be used in ggplot along with the ticks and labels
+  assign('ticks', ticks, envir=.GlobalEnv)
+  assign('labs',labs, envir= .GlobalEnv)
+  return(d)
+  
+}
+
+####IMPORT DATA
+
+##eQTL RESULTS
+complete_results <-read.table("~/Dropbox/Research/Lab Book/NGS/oink/microarray/eQTL_results_no-outliers/eQTL_results_LR_min3/cis_summary_86000.txt", h=T)
+
+complete_results$snp_chrom <- sub("chr", "", complete_results$snp_chrom)
+complete_results$snp_chrom <- sub("X", "20", complete_results$snp_chrom)
+qq_results <- data.frame("CHR" =as.integer(complete_results$snp_chrom), "BP" = as.integer(complete_results$snp_pos), "P" = as.numeric(complete_results$p.value), "SNP" = as.character(complete_results$snp_id))
+
+##Manhattan plot, no legend, with most significant labeled
+# ggplot(all) + geom_jitter(aes(x=snp_chrom, y=-log10(FDR), color=snp_chrom)) + geom_hline(yintercept=-log10(0.05)) + geom_hline(yintercept=-log10(0.001), linetype=3) + theme_bw() + xlab("") + theme(axis.text.x = element_text(angle=45, hjust=1, size=20), axis.text.y=element_text(size=20), legend.position="NONE", axis.title.y=element_text(size=20)) + scale_color_manual(values=c("#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a","#1f78b4", "#b2df8a")) + scale_y_continuous(breaks=seq(0:6))
+
+
+### Use qqman manhattan function to format the data appropriately for a Manhattan plot
+d <- manhattan(qq_results)
+
+##Change the chroms from numeric to factors, for colouring purposes.
+d$CHR <- as.factor(d$CHR)
+
+###"extra" data points that were added to set the pos values should be removed prior to plotting:
+#Requires the snp value "hide" for any additional "data" point that was added.
+d.real <- subset(d, d$SNP != 'hide')
+
+
+##GGPLOT MOD BY RUSSELL FRASER
+#scale_x_continous seems to break the x-axis of the facet_zoom
+#There's probably a smart workaround, but for now, the best I can come up with is saving a copy of the graph with
+# and without the sacle_x_continous as SVG and then merging the two. 
+
+ggplot(d) + geom_point(aes(x=pos, y=logp, color=CHR)) + theme_classic()+ ylab("-log(p)") #+ theme(axis.text.x=element_text(angle = 60, hjust=1, size=9), legend.title=element_blank(), text=element_text(size=20), plot.title=element_text(size=15), legend.position = "none") + scale_color_viridis(discrete=T) + geom_hline(yintercept=-log10(0.001), colour="red") #+ facet_zoom(x=CHR==1)+ facet_zoom(x=CHR==1 & BP > 88892979 & BP < 89006643) + scale_x_continuous(breaks=c(ticks), labels=labs)
+
+# ggsave("~/Dropbox/chrom.svg")
+
+# x=CHR==1 & BP > 88892979 & BP < 89006643
+# x=CHR==8 & BP > 40893025 & BP < 41121736
+
+#scale_color_manual(values=c("orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange", "navy blue","orange"))
+
