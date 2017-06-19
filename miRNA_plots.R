00003209727f65e8c19a7906f207620bc0c23f31
Russell Fraser <rfrase03@uoguelph.ca>
2017-06-19 09:11:38 -0400

    * R script to plot the equine miRNA in silico analysis.

--- miRNA_plots.R
+++ miRNA_plots.R
@@ -32,41 +32,57 @@ ggplot(snps, aes(x=V31)) + geom_histogram(aes(fill=V32), stat="count") + theme_c
 
 #####COMBINED#####
 
-default_full <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_default_full_with_gene_coords.txt", sep="\t")
-default_seed <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_default_seed_with_gene_coord.txt", sep="\t")
-stringent_full <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_stringent_full_with_gene_coord.txt", sep="\t")
-stringent_seed <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_stringent_seed_with_gene_coord.txt", sep="\t")
-compromise_full <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_compromise_full_with_gene_coords.txt", sep="\t")
-compromise_seed <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_compromise_seed_with_gene_coords.txt", sep="\t")
+default_full <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_default_full_final.vcf", sep="\t", h=T)
+default_seed <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_default_seed_final.vcf", sep="\t", h=T)
+stringent_full <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_stringent_full_final.vcf", sep="\t", h=T)
+stringent_seed <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_stringent_seed_final.vcf", sep="\t", h=T)
+compromise_full <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_compromise_full_final.vcf", sep="\t", h=T)
+compromise_seed <- read.table("~/equine/2014_11_24/miRNA/combined/targetscan_miranda_compromise_seed_final.vcf", sep="\t", h=T)
 
 df_list <- list(default_full, default_seed, stringent_full, stringent_seed, compromise_full, compromise_seed)
 
 refactor <- function(x){
-  x$V7 <- factor(x$V7, levels = c("2501 - 3000", "2001 - 2500", "1501 - 2000", "1001 - 1500", "501 - 1000", "<= 500"))
+  x$DISTANCE <- factor(x$DISTANCE, levels = c("2501 - 3000", "2001 - 2500", "1501 - 2000", "1001 - 1500", "501 - 1000", "<= 500"))
   x
 }
 
 df_list <- lapply(df_list, refactor)
 
 
-ggplot(df_list[[1]], aes(x=V6)) + geom_histogram(aes(fill=V7), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (default, full miR sequence)") + xlab("")
+ggplot(df_list[[1]], aes(x=GENE)) + geom_histogram(aes(fill=DISTANCE), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (default, full miR sequence)") + xlab("")+ scale_y_continuous(breaks=seq(-10,20,1))
 
-ggplot(df_list[[2]], aes(x=V6)) + geom_histogram(aes(fill=V7), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (default, seed miR sequence)") + xlab("")
+ggplot(df_list[[2]], aes(x=GENE)) + geom_histogram(aes(fill=DISTANCE), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (default, seed miR sequence)") + xlab("")+ scale_y_continuous(breaks=seq(-10,20,1))
 
-ggplot(df_list[[3]], aes(x=V6)) + geom_histogram(aes(fill=V7), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (stringent, full miR sequence)") + xlab("")
+ggplot(df_list[[3]], aes(x=GENE)) + geom_histogram(aes(fill=DISTANCE), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (stringent, full miR sequence)") + xlab("")+ scale_y_continuous(breaks=seq(-10,20,1))
 
-ggplot(df_list[[4]], aes(x=V6)) + geom_histogram(aes(fill=V7), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (stringent, seed miR sequence)") + xlab("")
+ggplot(df_list[[4]], aes(x=GENE)) + geom_histogram(aes(fill=DISTANCE), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (stringent, seed miR sequence)") + xlab("")+ scale_y_continuous(breaks=seq(-10,20,1))
 
-df_list[[1]]$V8 <- "def_full"
-df_list[[2]]$V8 <- "def_seed"
-df_list[[3]]$V8 <- "stringent_full"
-df_list[[4]]$V8 <- "stringent_seed"
-df_list[[5]]$V8 <- "compromise_full"
-df_list[[6]]$V8 <- "compromise_seed"
+ggplot(df_list[[5]], aes(x=GENE)) + geom_histogram(aes(fill=DISTANCE), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (compromise, full miR sequence)") + xlab("")+ scale_y_continuous(breaks=seq(-10,20,1))
+
+ggplot(df_list[[6]], aes(x=GENE)) + geom_histogram(aes(fill=DISTANCE), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (compromise, seed miR sequence)") + xlab("") + scale_y_continuous(breaks=seq(-10,20,1))
+
+
+df_list[[1]]$strategy <- "def_full"
+df_list[[2]]$strategy <- "def_seed"
+df_list[[3]]$strategy <- "stringent_full"
+df_list[[4]]$strategy <- "stringent_seed"
+df_list[[5]]$strategy <- "compromise_full"
+df_list[[6]]$strategy <- "compromise_seed"
 
 big_frame <- ldply(df_list, rbind)
 
-ggplot(big_frame, aes(x=V6)) + geom_histogram(aes(fill=V8), stat="count", position="dodge") + theme_classic() + theme(legend.position=c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + xlab("") + scale_fill_viridis(discrete=T)
+ggplot(big_frame, aes(x=GENE)) + geom_histogram(aes(fill=strategy), stat="count", position="dodge") + theme_classic() + theme(legend.position=c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + xlab("") + scale_fill_viridis(discrete=T) + scale_y_continuous(breaks=seq(-10,20,1))
 
-ggplot(big_frame, aes(x=V6)) + geom_histogram(aes(fill=V7), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (stringent, seed miR sequence)") + xlab("") + facet_wrap(~ V8)
+ggplot(big_frame, aes(x=GENE)) + geom_histogram(aes(fill=DISTANCE), stat="count") + theme_classic() + scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="BuGn") + theme(legend.position = c(1,1), legend.justification = c(1,1)) + ylab("# of SNVs") + ggtitle("Number of SNVs predicted by both TargetScan and miRanda (stringent, seed miR sequence)") + xlab("") + facet_wrap(~ DISTANCE)+ scale_y_continuous(breaks=seq(-10,20,1))
+
+ggplot(big_frame, aes(x=GENE)) + geom_bar(aes(y=miR_count, fill=strategy), stat="identity", position="dodge")
+
+
+
+###Tables
+aa<-compromise_seed[,c(1:5,31:34)]
+write.table(aa, file="~/Dropbox/compromise_seed.txt", sep="\t", row.names = F, quote=F)
+            
+colnames(compromise_seed)
+ncol(compromise_full)
 
