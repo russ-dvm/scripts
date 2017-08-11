0a5213bf353217722f0ed63799bd186641138d44
Russell Fraser <rfrase03@uoguelph.ca>
2017-08-11 15:03:24 -0400

    *Added two new flags:  --all: the default of the script is now to only output variant positions. The all flag outputs all (surprise) positions.  --snpeff: pulls the SnpEff annotation info and creats a new column, "variant_type".            snpeff flag needs work - snpeff annotates multiple genes at certain loci, meaning the annotation info that gets pulled is not always the relevant info.

--- add_variants_to_depth_of_coverage.py
+++ add_variants_to_depth_of_coverage.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 
 from __future__ import print_function
+from collections import defaultdict
 import argparse
+
 
 parser = argparse.ArgumentParser(description = "Take the output of <depth_of_coverage_by_region.py> and incporate information on variation from a VCF file. Will simply add a boolean to a new column 'is_variant'.", epilog = "RS Fraser, 2017/06/14")
 
 parser.add_argument("depth_file", help = "The output from <depth_of_coverage_by_region.py>")
 parser.add_argument("vcf_file", help = "A VCF file containing variants to be annotated.")
+parser.add_argument("--snpeff", help = "An optional to denote whether the vcf file has been run through SnpEff, and whether synnymous/non-synonymous info should be output in the annotation", action="store_true")
+parser.add_argument("--all", help = "When set, this will output all positions in the file, and not just the variants", action = "store_true")
+
 
 args = parser.parse_args()
 
@@ -16,17 +21,23 @@ vcf = open(args.vcf_file, 'r')
 
 #Create list of VCF positions
 vcf_list = []
+snpeff_dict = defaultdict(list)
 
 for entry in vcf:
 	entry = entry.rstrip()
-	if "##" in entry:
+	if "#" in entry:
 		continue
 	else:
 		entry_fields = entry.split("\t")
-
 		vcf_list.append(entry_fields[1])
+		annotation_field = entry_fields[7]
+		annotation_sub_field = annotation_field.split(";")
+		annotation_sub_field_1 = annotation_sub_field[3]
+		annotation_sub_field_2 = annotation_sub_field_1.split("|")
+		annotation_sub_field_3 = annotation_sub_field_2[1]
+		snpeff_dict[entry_fields[1]].append(annotation_sub_field_3)
 
-#Parse the annotated file and see if positions match
+# Parse the annotated file and see if positions match
 
 for line in depth:
 	line = line.rstrip()
@@ -34,10 +45,23 @@ for line in depth:
 
 	position = line_fields[1]
 
-	if "chrom" in line:
-		print(line, "is_variant", sep="\t")
-	else:
-		if position in vcf_list:
-			print(line, "TRUE", sep="\t")
+	if args.snpeff:
+		if "chrom" in line:
+			print(line, "is_variant", "variant_type", sep="\t")
 		else:
-			print(line, "FALSE", sep="\t")
+			if position in snpeff_dict:
+				print(line, "TRUE", snpeff_dict[position], sep="\t")
+			else:
+				if args.all:
+					print(line, "TRUE", "FALSE", sep = "\t")
+				
+	else:
+		if "chrom" in line:
+			print(line, "is_variant", sep="\t")
+		else:
+			if position in snpeff_dict:
+				print(line, "TRUE", sep="\t")
+			else:
+				if args.all:
+					print(line, "FALSE", sep="\t")
+
