1232ad5b5f6084f868ee773a6e8d15a9d01c73ab
Russell Fraser <rfrase03@uoguelph.ca>
2017-08-22 18:39:11 -0400

    stupid parse args

--- seed_or_full.py
+++ seed_or_full.py
@@ -2,3 +2,10 @@
 
 from __future__ import print_function
 import argparse
+
+!parser = argparse.ArgumentParser(description = "Description of the program to be written", epilog = "Name, date")
+
+parser.add_argument("input", help = "A file input, specification not required")
+parser.add_argument("-ref", dest = 'ref_file', help = "Requires the specification noted.")
+
+args = parser.parse_args()
