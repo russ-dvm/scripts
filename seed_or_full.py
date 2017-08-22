c73fa2e32a3a2799e95cf2f63b45f71908218152
Russell Fraser <rfrase03@uoguelph.ca>
2017-08-22 19:47:25 -0300

    early version
    just for testing

--- seed_or_full.py
+++ seed_or_full.py
@@ -3,9 +3,23 @@
 from __future__ import print_function
 import argparse
 
-!parser = argparse.ArgumentParser(description = "Description of the program to be written", epilog = "Name, date")
+!parser = argparse.ArgumentParser(description = "Assign "full" or "seed" status to SNPs depending on where they target MREs", epilog = "RS Fraser, 2017-08-22")
 
-parser.add_argument("input", help = "A file input, specification not required")
-parser.add_argument("-ref", dest = 'ref_file', help = "Requires the specification noted.")
+parser.add_argument("seed", help = "VCF file containing seed SNPs")
+parser.add_argument("full", help = "VCF file containing full-region SNPs")
 
 args = parser.parse_args()
+
+seed = open(args.seed, "r")
+full = open(args.full, "r")
+
+seed_list = []
+
+for line in seed:
+	line = line.rstrip()
+	line_fields = line.split("\t")
+
+	seed_loc = line_fields[1]
+	seed_list.append(seed_loc)
+
+print(seed_list)
