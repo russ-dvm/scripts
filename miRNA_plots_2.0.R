106dbcf2733771734b636f090ef8f2459bf8d63d
Russell Fraser <rfrase03@uoguelph.ca>
2017-08-23 11:35:17 -0300

    Plot for seed vs full
    Added comments and adjusted a few lines. Added a plot for the seed vs full SNPs

--- miRNA_plots_2.0.R
+++ miRNA_plots_2.0.R
@@ -1,13 +1,15 @@
 library(ggplot2)
 
-mirna_seed <- read.table("~/Dropbox/temp/miRNA/combined_by_mir_overlap/overlap.snps.named.txt", sep = "\t")
+mirna_seed <- read.table("~/Dropbox/temp/miRNA/combined_by_mir_overlap/overlap.snps.named.seed.txt", sep = "\t")
 mirna_full <- read.table("~/Dropbox/temp/miRNA/combined_by_mir_overlap/overlap.snps.full.named.txt", sep = "\t")
+mirna_ann <- read.table("~/Dropbox/temp/miRNA/combined_by_mir_overlap/overlap.seed_or_full.named.txt", sep = "\t")
 
-mirna$V33 <- factor(mirna$V33, levels = c("2501 - 3000", "2001 - 2500", "1501 - 2000", "1001 - 1500", "501 - 1000", "<= 500"))
-mirna$V33 <- factor(mirna$V33, levels = c("2501 - 3000", "2001 - 2500", "1501 - 2000", "1001 - 1500", "501 - 1000", "<= 500"))
+mirna_seed$V33 <- factor(mirna_seed$V33, levels = c("2501 - 3000", "2001 - 2500", "1501 - 2000", "1001 - 1500", "501 - 1000", "<= 500"))
+mirna_full$V33 <- factor(mirna_full$V33, levels = c("2501 - 3000", "2001 - 2500", "1501 - 2000", "1001 - 1500", "501 - 1000", "<= 500"))
+mirna_ann$V33 <- factor(mirna_ann$V33, levels = c("2501 - 3000", "2001 - 2500", "1501 - 2000", "1001 - 1500", "501 - 1000", "<= 500"))
 
-
-ggplot(mirna, aes(x=V32)) + 
+##SNPS THAT FALL INTO ONLY THE SEED AREA OF THE MRE
+ggplot(mirna_seed, aes(x=V32)) + 
   geom_histogram(aes(fill = V33), stat = "count") +
   theme_bw() +
   scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="RdPu") + 
@@ -18,7 +20,8 @@ ggplot(mirna, aes(x=V32)) +
   theme(panel.grid.minor.y = element_blank(), panel.grid.major.x = element_blank()) +
   ylab("Number of variants") +
   ggtitle("Number of variants predicted to fall within the seed region of an miRNA binding element predicted by both TargetScan and miRanda")
-       
+      
+## SNPS THAT FALL INTO THE FULL RANGE OF THE MRE
 ggplot(mirna_full, aes(x=V32)) + 
   geom_histogram(aes(fill = V33), stat = "count") +
   theme_bw() +
@@ -30,3 +33,16 @@ ggplot(mirna_full, aes(x=V32)) +
   theme(panel.grid.minor.y = element_blank(), panel.grid.major.x = element_blank()) +
   ylab("Number of variants") +
   ggtitle("Number of variants predicted to fall within an miRNA binding element predicted by both TargetScan and miRanda")
+
+###SEED OR FULL
+ggplot(mirna_ann, aes(x=V32)) + 
+  geom_histogram(aes(fill = V34), stat = "count", position = "dodge") +
+  theme_bw() +
+  # scale_fill_brewer(breaks=c("<= 500", "501 - 1000", "1001 - 1500", "1501 - 2000", "2001 - 2500", "2501 - 3000"), name="Distance from stop codon (bp)", palette="RdPu") + 
+  xlab("") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  ylim(0,10) +
+  scale_y_continuous(breaks = seq(0,28,1)) + 
+  theme(panel.grid.minor.y = element_blank(), panel.grid.major.x = element_blank()) +
+  ylab("Number of variants") +
+  ggtitle("")
