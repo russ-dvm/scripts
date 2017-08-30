2678b3396d4ace32246887459e228d2e268a1d97
Russell Fraser <rfrase03@uoguelph.ca>
2017-08-30 11:44:49 -0400

    Updated file names and produced a template for a publication ready table

--- popoolation_table_of_frequencies.R
+++ popoolation_table_of_frequencies.R
@@ -1,8 +1,7 @@
 library(tidyverse)
 
-freq_table <- read.table("~/equine/2014_11_24/popoolation/subsampled340_rc", h=T, stringsAsFactors = F)
-freq_table_2 <- read.table("~/equine/2014_11_24/popoolation/subsampled340-min20-noindels-for-rc_rc", h=T, stringsAsFactors = F)
-fisher <- read.table("~/equine/2014_11_24/popoolation/subsampled340-min20.sig-only.fet", col.names=c("Chrom", "pos", "A", "B", "C", "Pop", "invlogp"))
+freq_table <- read.table("~/equine/2014_11_24/popoolation/subsampled370_freqs_rc", h=T, stringsAsFactors = F)
+fisher <- read.table("~/equine/2014_11_24/popoolation/subsampled370.ready.fet", col.names=c("Chrom", "pos", "A", "B", "C", "Pop", "invlogp"))
 
 #Check the the major and minor alleles for each population are the same
 freq_table <- separate(freq_table, major_alleles.maa., c("maa_n", "maa_d"), sep = 1)
@@ -29,38 +28,46 @@ freq_table$maa_same <- freq_table$maa_n == freq_table$maa_d
 freq_table$mia_same <- freq_table$mia_n == freq_table$mia_d
 
 ##Good idea to inspect the table manually -- for the equine data there are a few SNPs that should be excluded and a couple that need to be manually fixed.
-#Entry 542 - normal population was entirely reference, thus the minor allele was not populated (weird)
-freq_table[542,]$mia_n <- "A"
-freq_table[542,]$mia_d <- "A"
-freq_table[2644,]$mia_d <- "A"
+freq_table[698,]$mia_d <- "A"
 freq_table$maa_same <- freq_table$maa_n == freq_table$maa_d
 freq_table$mia_same <- freq_table$mia_n == freq_table$mia_d
 freq_table <- freq_table[freq_table$mia_same == T,]
 
-##The above should remove these three
-# freq_table[-2557,]
-# freq_table[-3937,]
-# freq_table[-5556,]
 
 ##Merge the results of fisher's exact test with the actual frequencies
 mega_table <- merge(freq_table, fisher, by.x = "pos")
 mega_table_curated <- mega_table[c(-3, -4, -6, -7, -16:-22)]
 
 
-##Play with frequency diffs
-maybe <- data.frame("mia_n" = as.character(mega_table$mia_1), "mia_d" = as.character(mega_table$mia_2))
+####Manipulate the format of frequency diffs
+##Isolate the minor allele fractions
+maybe <- data.frame("mia_n" = as.character(mega_table$mia_1), "mia_d" = as.character(mega_table$mia_2), "pos" = as.numeric(mega_table$pos), "chrom" = as.character(mega_table$chr))
+##Split numerator and denominator
 maybe <- separate(maybe, mia_n, c("alt_n", "total_n"), sep = "/")
 maybe <- separate(maybe, mia_d, c("alt_d", "total_d"), sep = "/")
+##Convert back to numbers
 maybe$alt_n <- as.numeric(maybe$alt_n)
 maybe$alt_d <- as.numeric(maybe$alt_d)
 maybe$total_n <- as.numeric(maybe$total_n)
 maybe$total_d <- as.numeric(maybe$total_d)
-
+##Calculate the frequency
 maybe$n_freq <- maybe$alt_n/maybe$total_n
 maybe$d_freq <- maybe$alt_d/maybe$total_d
-
+##Calculate the difference in the frequencies
 maybe$diff <- maybe$n_freq - maybe$d_freq
+
+
 library(MASS)
 ggplot(maybe, aes(x = diff)) + geom_histogram(colour = "black", fill = "white") + theme_bw()
 qqnorm(maybe$diff) + qqline(maybe$diff)
-?qqnorm
+
+
+###Merge back into main table and create a final publication ready table
+
+mega_table_curated <- merge(mega_table_curated, maybe, by.x = "pos")
+
+final_table <- mega_table_curated[,c(2,1,4,6,10,11,18:20,12)]
+colnames(final_table) <- c("Chrom", "Position", "Ref allele", "Alt allele", "Frac in normal", "Frac in diseasd", "Freq in normal", "Freq in diseased", "Freq diff", "-log(p)")
+
+write.table(final_table, "~/equine/2014_11_24/popoolation/pub_table.txt", sep="\t", row.names=F, quote=F)
+?write.table
