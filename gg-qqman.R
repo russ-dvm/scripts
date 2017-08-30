43b03eabfe6d5c0a81dacfd73fcdfc66c66e2e41
Russell Fraser <rfrase03@uoguelph.ca>
2017-08-30 15:01:09 -0400

    Tweaks for updated file names, also can now directly add start/stop in R

--- gg-qqman.R
+++ gg-qqman.R
@@ -6,11 +6,10 @@ library(ggforce)
 
 ###STEPHEN TURNER'S CODE FROM 
 ## https://github.com/stephenturner/qqman/blob/master/R/manhattan.R
-
 manhattan <- function(x, chr="CHR", bp="BP", p="P", snp="SNP", 
                       col=c("gray10", "gray60"), chrlabs=NULL,
                       suggestiveline=-log10(1e-5), genomewideline=-log10(5e-8), 
-                      highlight=NULL, logp=TRUE, annotatePval = NULL, annotateTop = TRUE, ...) {
+                      highlight=NULL, logp=TRUE, annotatePval = NULL, annotateTop = TRUE, X=NULL, Y=NULL, ...) {
   
   # Not sure why, but package check will warn without this.
   CHR=BP=P=index=NULL
@@ -71,11 +70,13 @@ manhattan <- function(x, chr="CHR", bp="BP", p="P", snp="SNP",
     #d$pos=d$BP/1e6
     d$pos=d$BP
     ticks=floor(length(d$pos))/2+1
+    minor_ticks = floor(length(d$pos))/2+1
     xlabel = paste('Chromosome',unique(d$CHR),'position')
     labs = ticks
   } else { ## For multiple chromosomes
     lastbase=0
     ticks=NULL
+    minor_ticks=NULL
     for (i in unique(d$index)) {
       if (i==1) {
         d[d$index==i, ]$pos=d[d$index==i, ]$BP
@@ -88,10 +89,17 @@ manhattan <- function(x, chr="CHR", bp="BP", p="P", snp="SNP",
       # New way: doesn't make that assumption
       ticks = c(ticks, (min(d[d$index == i,]$pos) + max(d[d$index == i,]$pos))/2 + 1)
       
+      ##MOD: ticks that bound the chromosome, rather than appear at its center - RSF
+      minor_ticks = c(minor_ticks, min(d[d$index == i,]$pos, max(d[d$index == i,]$pos)))
+      
     }
     xlabel = 'Chromosome'
     #labs = append(unique(d$CHR),'') ## I forgot what this was here for... if seems to work, remove.
     labs <- unique(d$CHR)
+    
+    ##MOD - use the X and Y options to rename the X and Y chromosomes to the more appropriate "X" and "Y". This does coerce labs into character instead of numeric but that doesn't seem to affect the output of ggplot2. RSF.
+    labs[X] <- "X"
+    labs[Y] <- "Y"
   }
   
   # Initialize plot
@@ -101,25 +109,27 @@ manhattan <- function(x, chr="CHR", bp="BP", p="P", snp="SNP",
   ###MODIFIED HERE -- Direct ticks and labels into their own values and return the data frame 
   ## The datafrmae (d) can then be used in ggplot along with the ticks and labels
   assign('ticks', ticks, envir=.GlobalEnv)
-  assign('labs',labs, envir= .GlobalEnv)
+  assign('labs', labs, envir= .GlobalEnv)
+  assign('minor_ticks', minor_ticks, envir= .GlobalEnv)
   return(d)
   
 }
 
 ####IMPORT DATA
+## Raw equine data
+popoolation <- read.table("~/equine/2014_11_24/popoolation/subsampled370.ready.fet", col.names=c("Chrom", "Position", "A", "B", "C", "Pop", "invlogp"))
+start_stop <- read.table("~/equine/2014_11_24/popoolation/start_stop_for_qqman.txt", sep = "\t", h = T)
 
-##EQUINE
-results <- read.table('~/playground/qqman/results2.txt', h=T)
+## Reformat for qqman -- expects three columns, Chr, Bp, P, and SNP-id
+man_frame <- data.frame("CHR" = popoolation$Chrom, "BP" = as.integer(popoolation$Position), "P" = 10^(-popoolation$invlogp), "SNP" = rep("snp", nrow(popoolation)))
+man_frame$CHR <- gsub("chr", "", man_frame$CHR)
+man_frame$CHR <- as.numeric(man_frame$CHR)
 
-##BOVINE
-#results <- read.table("", h=T)
-
-
-##Popoolation2 P values have already been -log, qqman expects raw P values, so have to re-calculate raw P
-results$P <- 10^(-results$P)
+##Add in start and stop "snps" - pvalue 0 - and snp value of hide. This is necessary so that the chr widths are accurate.
+man_frame <- rbind(man_frame, start_stop)
 
 ### Use qqman manhattan function to format the data appropriately for a Manhattan plot
-d <- manhattan(results)
+d <- manhattan(man_frame)
 
 ###Determine BH significance levels###
 #order data by p value, from lowest to highest
@@ -129,10 +139,9 @@ rownum <- nrow(sorted.by.p)
 #assign ranks to each row, lowest p value = 1
 sorted.by.p$rank <- c(1:rownum) 										
 #set the false discovery rate
-fdr = 0.01																
+fdr = 0.000001																
 #calculate the benjamini hochberg critical value - the integer = False Discovery Rate
 benj.hoch <- (sorted.by.p$rank/rownum)*fdr 		
-#total #in n_D.fet = 1075951
 
 #add benjamini hochbergs to the data frame
 sorted.by.p$bh <- benj.hoch 							
@@ -152,13 +161,10 @@ lowest.significant.row <- lowest.significant$rank
 #inverse log of the p-value of highest ranked sig value
 intercept <- lowest.significant$P				
 
-##Change the chroms from numeric to factors, for colouring purposes.
+
+####Change the chroms from numeric to factors, for colouring purposes.
 d$CHR <- as.factor(d$CHR)
 
-
-###"extra" data points that were added to set the pos values should be removed prior to plotting:
-#Requires the snp value "hide" for any additional "data" point that was added.
-d.real <- subset(d, d$SNP != 'hide')
 
 
 ##GGPLOT MOD BY RUSSELL FRASER
@@ -166,7 +172,21 @@ d.real <- subset(d, d$SNP != 'hide')
 #There's probably a smart workaround, but for now, the best I can come up with is saving a copy of the graph with
 # and without the sacle_x_continous as SVG and then merging the two. 
 
-ggplot(d) + geom_point(aes(x=pos, y=logp, color=CHR)) + theme_classic()+ ylab("-log(p)") + theme(axis.text.x=element_text(angle = 60, hjust=1, size=9), legend.title=element_blank(), text=element_text(size=20), plot.title=element_text(size=15), legend.position = "none") + scale_color_viridis(discrete=T) + geom_hline(yintercept=-log10(intercept), colour="red") + facet_zoom(x=CHR==1)+ facet_zoom(x=CHR==1 & BP > 88892979 & BP < 89006643) + scale_x_continuous(breaks=c(ticks), labels=labs)
+ggplot(d) + 
+  geom_point(aes(x=pos, y=logp, color=CHR)) + 
+  theme_bw() +
+  theme(panel.grid.minor.x = element_line(colour = "light grey"), panel.grid.major.x = element_blank()) +
+  theme(panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank()) +
+    ylab("-log(p)") + 
+  theme(axis.text.x=element_text(angle = 60, hjust=1, size=9), legend.title=element_blank(), text=element_text(size=20), plot.title=element_text(size=15), legend.position = "none") + 
+  scale_color_viridis(discrete=T) + 
+  geom_hline(yintercept=-log10(intercept), colour="red") + 
+  scale_x_continuous(minor_breaks = minor_ticks, breaks = ticks, labels = labs) +
+  # scale_x_continuous(breaks=c(ticks), labels=labs) #+
+  facet_zoom(x=CHR==1)+ facet_zoom(x=CHR==1 & BP > 88892979 & BP < 89006643)
+  
+sig_only <- subset(sorted.by.p, sorted.by.p$significant == T)
+nrow(sig_only)
 
 # ggsave("~/Dropbox/chrom.svg")
 
