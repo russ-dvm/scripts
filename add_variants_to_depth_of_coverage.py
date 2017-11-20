6769771d2a229010fd8bab65f9e864c5a7b2d04d
Russell Fraser <rfrase03@uoguelph.ca>
2017-11-20 11:03:00 -0500

    Can now handle situations in which the same different chromosomes have the same target positions.

--- add_variants_to_depth_of_coverage.py
+++ add_variants_to_depth_of_coverage.py
@@ -21,6 +21,8 @@ vcf = open(args.vcf_file, 'r')
 
 #Create list of VCF positions
 vcf_list = []
+
+#Empty dictionary to associate a snpeff annotation to the variant position
 snpeff_dict = defaultdict(list)
 
 for entry in vcf:
@@ -29,13 +31,19 @@ for entry in vcf:
 		continue
 	else:
 		entry_fields = entry.split("\t")
-		vcf_list.append(entry_fields[1])
+
+		# In bovine expt, there are overlapping coords on diff chroms (this wasn't a problem with equine). Needs a mech to ensure that position 15 on chr10 doesn't match position 15 on chr20. Easy solution - concatenate the chrom and position strings to make a unique identifier - eg chr10:15. This style also matches Ensembl/UCSC so I think we'll use it.
+
+		chrom = entry_fields[0]
+		var_pos = entry_fields[1]
+		var_entry = chrom + ":" + var_pos
+		vcf_list.append(var_entry)
 		annotation_field = entry_fields[7]
 		annotation_sub_field = annotation_field.split(";")
 		annotation_sub_field_1 = annotation_sub_field[3]
 		annotation_sub_field_2 = annotation_sub_field_1.split("|")
 		annotation_sub_field_3 = annotation_sub_field_2[1]
-		snpeff_dict[entry_fields[1]].append(annotation_sub_field_3)
+		snpeff_dict[var_entry].append(annotation_sub_field_3)
 
 # Parse the annotated file and see if positions match
 
@@ -43,14 +51,17 @@ for line in depth:
 	line = line.rstrip()
 	line_fields = line.split("\t")
 
+	chrom = line_fields[0]
 	position = line_fields[1]
+
+	depth_entry = chrom + ":" + position
 
 	if args.snpeff:
 		if "chrom" in line:
 			print(line, "is_variant", "variant_type", sep="\t")
 		else:
-			if position in snpeff_dict:
-				print(line, "TRUE", snpeff_dict[position], sep="\t")
+			if depth_entry in snpeff_dict:
+				print(line, "TRUE", snpeff_dict[depth_entry], sep="\t")
 			else:
 				if args.all:
 					print(line, "FALSE", "FALSE", sep = "\t")
@@ -59,7 +70,7 @@ for line in depth:
 		if "chrom" in line:
 			print(line, "is_variant", sep="\t")
 		else:
-			if position in snpeff_dict:
+			if depth_entry in snpeff_dict:
 				print(line, "TRUE", sep="\t")
 			else:
 				if args.all:
